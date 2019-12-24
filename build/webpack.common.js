c54915f0297456d520dea7e5b9b9af737d2f6920
高洪城 <18367733277@163.com>
2019-12-24 10:04:02 +0800

    配置source map

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,16 +1,11 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        contentBase: './dist'
+        path: path.resolve(__dirname, '../dist')
     },
     module: {
         rules: [
@@ -24,7 +19,6 @@ module.exports = {
         ]
     },
     plugins: [
-        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: 'src/index.html'
         })
