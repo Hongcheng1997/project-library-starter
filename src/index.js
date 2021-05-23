fe1f4956acd5139a1c960b01677d468b232c2b06
gaohongcheng <18367733277@163.com>
2021-05-23 22:14:55 +0800

    feat: BundleAnalyzerPlugin、eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 class MyWebpack {
   constructor() {
-    this.name = 'ghc1'
-    console.log(this.getName())
+    this.name = 'ghc1';
+    console.log(this.getName());
   }
 
   getName() {
-    return this.name
+    return this.name;
   }
 }
 
-new MyWebpack()
+new MyWebpack();
